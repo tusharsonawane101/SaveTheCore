56fa706bcee0e6fd8644958a77cf7978b29c42c7
tusharsonawane101 <39156808+tusharsonawane101@users.noreply.github.com>
2018-05-15 21:58:56 +0530

    Add files via upload

---
+++ Spawner/circularPath_2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class circularPath_2 : MonoBehaviour {
+	public float timeCounter;
+	float speed;
+	public float width;
+	public float height;
+	// Use this for initialization
+	void Start () {
+		//speed = 5;
+		//	width = 4;
+		//	height = 7;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime;
+		
+		float x = Mathf .Sin (timeCounter)*width;
+		float y = Mathf. Cos(timeCounter)*height;
+		
+		transform .position = new Vector2 (x, y);
+	}
+}
+
+////TKS
